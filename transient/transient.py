f067bfd10b95882531bae8c19fe95c3bb5ff1263
Ramin <ramin.mh.007@gmail.com>
2025-06-19 20:20:03 +0530

    Improved the code

--- transient/transient.py
+++ transient/transient.py
@@ -1,31 +1,28 @@
 # this program makes a relation transitive 
 
-relation = [("c", "a"), ("d", "e"), ("b", "a"), ("e", "f"), ("a", "e")]
-relation_new = relation.copy()
+given_relation = {("c", "a"), ("d", "e"), ("b", "a"), ("e", "f"), ("a", "e")}
 
-def find_transitive(length):
-    for i in range(length):
-        for j in range(length):
-            if not j == i:
-                if relation_new[i][1] == relation_new[j][0]:
-                    if (relation_new[i][0], relation_new[j][1]) not in relation_new:
-                        relation_new.append((relation_new[i][0], relation_new[j][1]))
+def find_transitive(relation):
+    new_relation = set(relation)
+    added = True
 
-                        return 1
-    
-    return 0
+    while added:
+        added = False
+        new_pairs = set()
 
+        for a, b in new_relation:
+            for c, d in new_relation:
+                if b == c and (a, d) not in new_relation:
+                    new_pairs.add((a,d))
+                    added = True
+        
+        new_relation.update(new_pairs)
+
+    return new_relation
 
 if __name__ == '__main__':
-    while True:
-        if find_transitive(len(relation_new)) == 1:
-            continue
-        else:
-            break
-
-
     print("------------------------------")
-    print(f"Given relation: {relation}")
+    print(f"Given relation: {given_relation}")
     print("===========================================================================")
-    print(f"Transitive relation: {relation_new}")
+    print(f"Transitive relation: {find_transitive(given_relation)}")
     print("------------------------------")
